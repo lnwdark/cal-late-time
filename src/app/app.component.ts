a0056b18a9226ff889bc639a2e9afbd1db722d37
lnwdark <04061surichai@gmail.com>
2020-08-11 10:21:36 +0700

    deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-bulma';
+  title = 'คำนวนเงินมาสาย';
+
+  total: number = 0;
+  salary: number;
+  amount: number;
+
+  cal(): void {
+    if (this.salary && this.amount) {
+      const sum = (this.salary / 30) / 8 * this.amount;
+      console.log(sum);
+      this.total = sum;
+    }
+
+  }
+
+  numberOnly(event): boolean {
+    const charCode = (event.which) ? event.which : event.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+      return false;
+    }
+    return true;
+  }
 }
